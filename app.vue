47605b09d83dbf2a781f31ce75738f4e58756675
Torn-Soontorn <conan_torn@hotmail.com>
2023-02-25 17:34:24 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+<div class="flex justify-center items-center h-screen">
+    <h1 class="text-3xl font-bold text-purple-600 animate-bounce">
+      Hello Nuxt 3
+    </h1>
+</div>
+</template>
